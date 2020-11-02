0b12f9d245a85bfa3ad2de59ac2c3f06b448acf0
Adam Sztandar <sztandaradam@gmail.com>
2020-11-02 13:11:34 +0100

    Add Task 9 & Task 10

--- lab2/js/my/zadania.js
+++ lab2/js/my/zadania.js
@@ -1,33 +1,33 @@
 //zadanie 1
-function zamienKolorLogoZielony(){
-    document.getElementById("logo").style.backgroundColor="green";
+function zamienKolorLogoZielony() {
+    document.getElementById("logo").style.backgroundColor = "green";
 }
-function zamienKolorLogokDomyslny(){
-    document.getElementById("logo").style.backgroundColor= "#428BCA";
+function zamienKolorLogokDomyslny() {
+    document.getElementById("logo").style.backgroundColor = "#428BCA";
 }
 
 //zadanie2
 var zamienKolorLogoZielonyDomyslnyBoolean = 0;
 var kolor = "";
 var textButton = "";
-function zamienKolorLogoZielonyDomyslny(){
-    if(zamienKolorLogoZielonyDomyslnyBoolean == 0){
+function zamienKolorLogoZielonyDomyslny() {
+    if (zamienKolorLogoZielonyDomyslnyBoolean == 0) {
         kolor = "green"
         zamienKolorLogoZielonyDomyslnyBoolean = 1;
         textButton = "Zmień kolor na domyślny";
     }
-    else{
+    else {
         kolor = "#428BCA";
         zamienKolorLogoZielonyDomyslnyBoolean = 0;
         textButton = "Zmień kolor na zielony";
     }
-    document.getElementById("logo").style.backgroundColor= kolor;
+    document.getElementById("logo").style.backgroundColor = kolor;
     document.getElementById("buttonZadanie2").textContent = textButton;
 }
 
 //zadanie 3
 
-function klonujLogo(){
+function klonujLogo() {
     var logo = document.querySelector(".logoClone");
     var klonLogo = logo.cloneNode(true);
     logo.appendChild(klonLogo);
@@ -36,7 +36,7 @@ function klonujLogo(){
 
 //zadanie 4
 
-function usunLogo(){
+function usunLogo() {
 
     var elementy = document.querySelector(".logoClone");
     elementy.remove();
@@ -44,18 +44,18 @@ function usunLogo(){
 
 //zadanie 5
 
-function pokolorujTloNaLosowyKolor(){
-    var koloryTab = ['green','yellowy','blue','red','pink'];
+function pokolorujTloNaLosowyKolor() {
+    var koloryTab = ['green', 'yellowy', 'blue', 'red', 'pink'];
     var koloryTabLen = koloryTab.length;
 
-    var wylosowanaLiczba = Math.floor(Math.random() * (koloryTabLen-1 - 0)) + 0;
+    var wylosowanaLiczba = Math.floor(Math.random() * (koloryTabLen - 1 - 0)) + 0;
     document.body.style.backgroundColor = koloryTab[wylosowanaLiczba];
-    
+
 }
 
 //zadanie 6
 
-function zmienWielkoscCzcionki(){
+function zmienWielkoscCzcionki() {
 
     var elementy = document.getElementById("zadanie6p");
     elementy.style.fontSize = "60px";
@@ -63,20 +63,53 @@ function zmienWielkoscCzcionki(){
 //zadanie 7
 
 
-    var suwak = document.getElementById("myRange");
-    var czcionkaP = document.getElementById("zadanie7p");
-    var wielkosc,rozmiar;
-    function zmienWielkoscCzcionkiSuwak(){
-         wielkosc =  suwak.value;
-        rozmiar = wielkosc + "px;";
-        czcionkaP.style.fontSize = suwak.value + "px";
-    }
+var suwak = document.getElementById("myRange");
+var czcionkaP = document.getElementById("zadanie7p");
+var wielkosc, rozmiar;
+function zmienWielkoscCzcionkiSuwak() {
+    wielkosc = suwak.value;
+    rozmiar = wielkosc + "px;";
+    czcionkaP.style.fontSize = suwak.value + "px";
+}
 
-    //zadanie 8
-    let curr = 0;
-    function zadanie8funkcja(){
-        "use strict";
-        let x = document.getElementsByClassName("badge-light");
-        curr = curr+6;
-        x[0].innerHTML = curr;
-    }
+//zadanie 8
+let curr = 0;
+function zadanie8funkcja() {
+    "use strict";
+    let x = document.getElementsByClassName("badge-light");
+    curr = curr + 6;
+    x[0].innerHTML = curr;
+}
+
+//zadanie 9
+var numer = Math.floor(Math.random() * 5) + 1;
+
+var timer1 = 0;
+var timer2 = 0;
+
+function ustawslajd(nrslajdu) {
+    clearTimeout(timer1);
+    clearTimeout(timer2);
+    numer = nrslajdu - 1;
+
+    schowaj();
+    setTimeout("zmienslajd()", 500);
+}
+
+function schowaj() {
+    $("#slider").fadeOut(500);
+}
+
+function zmienslajd() {
+    numer++; if (numer > 5) numer = 1;
+
+    var plik = "<img src=\"img/img" + numer + ".jpg\" class=\"sliderImg\"/>";
+
+    document.getElementById("slider").innerHTML = plik;
+    $("#slider").fadeIn(500);
+
+    timer1 = setTimeout("zmienslajd()", 5000);
+    timer2 = setTimeout("schowaj()", 4500);
+}
+
+zmienslajd();
